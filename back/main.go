de23bac91b1d299d1fde6c796a17048325d1a2ad
gojiteji <mail@gojiteji.com>
2020-05-08 11:24:30 +0900

    host and guest connection

--- back/main.go
+++ back/main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"./pages"
+	"math/rand"
+	"time"
+
+	//"./pages"
+	"github.com/jinzhu/gorm"
 )
 
-func acces(session_id string,ctx *gin.Context){
-		ctx.HTML(200, "index.html", gin.H{"session_id": session_id})
+
+type User struct{
+	 gorm.Model
+	 Id string
+	 Name string
+	 IsHost bool
 }
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var rs1Letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandString1(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = rs1Letters[rand.Intn(len(rs1Letters))]
+	}
+	return string(b)
+}
+
+
+
 
 
 func main() {
@@ -17,24 +41,60 @@ func main() {
 
 	//title
 	router.GET("/", func(ctx *gin.Context){
-		pages.Generate_title(ctx)
+		ctx.HTML(200, "index.html", gin.H{"page":0})
 	})
 
-	//Create
-	router.POST("/new", func(ctx *gin.Context) {
+	//CreateRoom
+	router.POST("/Create", func(ctx *gin.Context) {
 		ctx.Redirect(302, "/")
-		pages.Createroom(ctx)
+		ctx.HTML(200, "index.html", gin.H{"page":1,"Name":"gamehost"})
 	})
 
-
-
-	router.GET("/play/:session_id", func(ctx *gin.Context){
-		session_id:= ctx.Param("session_id")
-		fmt.Print("session_id is")
-		fmt.Print(session_id)
-		acces(session_id,ctx)
+	//Created
+	router.POST("/Created", func(ctx *gin.Context) {
+		//roomidを生成
+		id :=RandString1(20)
+		var host User
+		host.Id=ctx.Param("session_id")
+		host.Name=ctx.PostForm("text")
+		host.IsHost=true
+		//hostをサーバーに保存
+		ctx.Redirect(302, "/play/"+string(id))
 	})
 
+	//JoinGame
+	router.POST("/Join", func(ctx *gin.Context){
+		ctx.HTML(200, "index.html", gin.H{"page":2,"Name":"guest"})
+	})
 
+	router.POST("/Joined", func(ctx *gin.Context) {
+		//guestをサーバーに保存
+		id:=ctx.PostForm("id")
+		ctx.Redirect(302, "/play/"+id)
+	})
+
+	//Room page
+	router.GET("/play/:session_id", func(ctx *gin.Context) {
+
+		//使えるサーバーかどうか(ログイン済みか?参加ユーザか?)
+		available:=true
+
+		//guest
+		var guest User
+		guest.Id=ctx.Param("id")
+		guest.Name=ctx.PostForm("text")
+		guest.IsHost=false
+
+		//host
+		var host User
+		host.Id=ctx.Param("session_id")
+		host.Name="hostgame"
+		host.IsHost=true
+
+		//同一セッション内のユーザを全て取得
+		Users:= [...] User{host, host,host}
+
+		ctx.HTML(200, "waiting.html", gin.H{"host": host,"available":available,"Users":Users})
+	})
 	router.Run()
 }
--- back/pages/pages.go
+++ back/pages/pages.go
@@ -1,41 +1,6 @@
 package pages
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
+
 )
 
-type Info struct {
-	gorm.Model
-	Players   int
-	Page int
-}
-
-type Host struct {
-	gorm.Model
-	id   int
-	name string
-}
-
-func Generate_title(ctx *gin.Context) {
-	var info Info
-	info.Players=100
-	info.Page=0
-	ctx.HTML(200, "index.html", gin.H{"info":info})
-}
-
-func Createroom(ctx *gin.Context){
-	//redisにid登録
-	var host Host
-	host.id=777
-	host.name="koki"
-	ctx.HTML(200, "index.html", gin.H{"host":host})
-}
-
-func Joinroom(ctx *gin.Context){
-	//redisにid登録
-	var host Host
-	host.id=777
-	host.name="koki"
-	ctx.HTML(200, "index.html", gin.H{"host":host})
-}
